c518ec4165536de42bcf92cfb5bc2caaacbe0990
MayankSrivastava28 <mayank945377@gmail.com>
2024-05-27 17:42:44 +0530

    added quiz service and wrote sql query to fetch data

--- src/main/java/com/mayank/quiz/DAO/QuestionDAO.java
+++ src/main/java/com/mayank/quiz/DAO/QuestionDAO.java
@@ -3,6 +3,7 @@ package com.mayank.quiz.DAO;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mayank.quiz.DTO.Question;
@@ -11,5 +12,9 @@ import com.mayank.quiz.DTO.Question;
 public interface QuestionDAO extends JpaRepository<Question, Integer>{
 //	public List<Question> findAll();
 	public List<Question> findByCategory(String category);
+
+	@Query(value = "select * from question q where q.category=:category order by Random() "
+			+ "LIMIT :numQ", nativeQuery = true)
+	public List<Question> findRandomQuestionsByCategory(String category, int numQ);
 
 }
--- src/main/java/com/mayank/quiz/Service/QuizService.java
+++ src/main/java/com/mayank/quiz/Service/QuizService.java
@@ -1,10 +1,15 @@
 package com.mayank.quiz.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.mayank.quiz.DAO.QuestionDAO;
 import com.mayank.quiz.DAO.QuizDao;
+import com.mayank.quiz.DTO.Question;
 import com.mayank.quiz.DTO.Quiz;
 
 @Service
@@ -12,12 +17,18 @@ public class QuizService {
 	
 	@Autowired
 	QuizDao quizdao;
+	@Autowired
+	QuestionDAO  questionDao;
 
 	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
 		
+		List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+		
 		Quiz quiz = new Quiz();
-//		quiz.setTitle(title);
-		return null;
+		quiz.setTitle(title);
+		quiz.setQuestions(questions);
+		quizdao.save(quiz);
+		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
 	}
 
 }
